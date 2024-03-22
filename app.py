24cf57d53526cce52e35f984e3e93d17027a47a4
zdravko7 <zbotushanov@gmail.com>
2024-03-22 16:50:24 +0200

    Adding initial files

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/', methods=['GET'])
+def home():
+    temperature = weather_controller.print_current_temperature()
+    return render_template('home.html', temperature=temperature)
+
+
+@app.route('/contact', methods=['GET'])
+def contact():
+    return "Contact page"
+    
+if __name__ == "__main__":
+    app.run()
