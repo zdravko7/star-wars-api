42d9229e9cf7e91a8ea5cd611d909e384067dc5d
zdravko7 <zbotushanov@gmail.com>
2024-03-22 16:54:18 +0200

    fixed an issue with old code

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+
 from flask import Flask, render_template
-app = Flask(__name__)#template_folder='templates')
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/', methods=['GET'])
 def home():
-    return "kur"
+    return render_template('home.html')
 
 
 @app.route('/contact', methods=['GET'])
