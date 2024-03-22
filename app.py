373f016890d3fbdbb8256ceb22d283f46f25759a
zdravko7 <zbotushanov@gmail.com>
2024-03-22 16:51:50 +0200

    fixed a bug

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)#template_folder='templates')
 
 @app.route('/', methods=['GET'])
 def home():
-    temperature = weather_controller.print_current_temperature()
-    return render_template('home.html', temperature=temperature)
+    return "kur"
 
 
 @app.route('/contact', methods=['GET'])
